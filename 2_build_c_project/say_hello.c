42ff3c3644466a26bffc9f4d8c7e7d7243521a63
Gennady Portenko <gportenko@gmail.com>
2018-08-29 10:20:55 +0400

    Added 1_copy_file/ Added 2_build_c_project/ Added 3_build_cpp_project/

---
+++ 2_build_c_project/say_hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
+void say_hello() {
+  printf("hello, pi = %4.2f \n", M_PI);
+}
---
+++ 3_build_cpp_project/say_hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <math.h>
+
+void say_hello() {
+  std::cout << "hello, pi = " <<  M_PI << std::endl;
+}
